7777730004872833e01f79f698f388dbad43e0ea
liho-o <daniil200120@mail.ru>
2024-05-09 14:35:00 +0700

    5

---
+++ 5/solution.py
@@ -0,0 +1,21 @@
+
+def main(first, second, third):
+    try:
+        result = first / second + third
+        out = open("output", 'w')
+        out.write(str(result))
+        out.close()
+    except ZeroDivisionError:
+        print('division by 0')
+
+
+f = open('input', 'r')
+rows = f.read().split('\n')
+text = ''
+
+try:
+    first, second, third = map(int, (rows[0], rows[1], rows[2]))
+    main(first, second, third)
+    f.close()
+except ValueError:
+    print('data eror')
